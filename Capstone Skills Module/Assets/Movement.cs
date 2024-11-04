46c9a1dd2acc4e3d717361fb587aafe9a9d1e1a6
Stigilzz <kmsalik@asu.edu>
2024-11-03 18:26:32 -0700

    Added Level And Finished Movement

--- Capstone Skills Module/Assets/Movement.cs
+++ Capstone Skills Module/Assets/Movement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(-10,0,0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(10, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0, 0, -10) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 0, 10) * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.GetComponent<Rigidbody>().AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+        }
     }
 }
